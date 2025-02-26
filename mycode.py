73358486e453eb79a6cc759598dd59b835552ab2
loharprathmesh11 <loharprathmesh11@gmail.com>
2025-02-26 10:03:22 +0530

    version:3 with data change

--- mycode.py
+++ mycode.py
@@ -9,13 +9,17 @@ data = {'Name': ['Alice', 'Bob', 'Charlie'],
 
 df = pd.DataFrame(data)
 
-# # Adding new row to df for V2
 new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
 df.loc[len(df.index)] = new_row_loc
 
-# # Adding new row to df for V3
+
 new_row_loc2 = {'Name': 'GF2', 'Age': 30, 'City': 'City2'}
 df.loc[len(df.index)] = new_row_loc2
+
+# Adding new row to df for V2
+new_row_locv3 = {'Name': 'GFv3', 'Age': 23, 'City': 'Cityv3'}
+df.loc[len(df.index)] = new_row_locv3
+
 
 # Ensure the "data" directory exists at the root level
 data_dir = 'data'
